9f0778d617aad3ba804dbb034d72d8e52b3b480f
Godson1117 <godsonsojan@gmail.com>
2024-06-22 03:54:51 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const qs = require('./questions.json');
+const app = express();
+const port = 8000;
+
+app.use(cors());
+
+app.get('/:topic', (req, res) => {
+    const randomNum = Math.floor(Math.random() * 10);
+    const topic = req.params.topic;
+
+    let questions;
+    if (topic === 'Education') {
+        questions = qs.educationQuestions;
+    } else if (topic === 'Health') {
+        questions = qs.healthQuestions;
+    } else if (topic === 'Technology') {
+        questions = qs.technologyQuestions;
+    } else {
+        res.status(404).json({ error: 'Invalid topic' });
+        return;
+    }
+    const randomQuestions = questions.slice(0, randomNum);
+    res.json(randomQuestions);
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
